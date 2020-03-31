b74fd5198bf86a6ed6cd4d2c0aa153656e44a24d
RobbyMc <rmm64@njit.edu>
2020-03-30 23:41:08 -0400

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,71 @@
+import java.lang;
+import java.util.*;
+public class Graph {
+
+ class Node{
+     String item;
+     Node next;
+     Node head = null;
+     Node tail = null;
+    public Node(String nodeVal, Node next){
+       this.next = next;
+       this.item = null;
+      
+    }
+  }
+
+ Node current;
+ private Node vertices;
+  
+  public void addNode(final String nodeVal){
+        
+        if(current != null)
+          {
+            Node newNode = new Node(nodeVal,current.next);
+            current.next = newNode;
+            current = newNode;
+          }
+          else
+            {
+              head = tail = new Node(nodeVal,null );
+              current = head;
+              }
+     }
+     
+     
+     
+  public void addUndirectedEdge(final Node first,final Node second){
+       Node v1 = vertices;
+       Node v2 = vertices;
+       
+       while(vertices != null){
+          if(first.equals(vertices.item)){
+             v1 = vertices;
+           }
+           if(second.equals(vertices.item)){
+             v2 = vertices;
+             }
+             vertices = vertices.next;
+             }
+    
+    }
+    
+    
+  public  void removeUndirectedEdge(final Node first, Node second){
+        return removeUndirectedEdge(new Node(first,last));
+  
+  
+  }
+  
+  public HashSet<Node> getAllNodes(){
+     HashSet<Node> numNodes = new HashSet<Node>();
+     return numNodes;
+  
+  
+  }
+  
+  }
+  
+  
+  
+  
