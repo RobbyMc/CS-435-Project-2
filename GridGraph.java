7f717e26cb453670c543916b6ba2d278fe7b6e1e
RobbyMc <rmm64@njit.edu>
2020-04-06 23:50:52 -0400

    Add files via upload

---
+++ GridGraph.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.lang.io;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class GridGraph {
+
+ class Node{
+
+  Node current;
+  string item;
+  
+
+  }
+
+public void addGridNode(final int x, final int y, final String nodeVal){
+  Node head = new Node(int data);
+     if(head==null)
+            return new Node(data);
+        else{
+            Node nxt = head;
+            while(nxt!=null)
+                nxt = nxt.next;
+            nxt = new Node(data);
+            return head;
+        }
+        
+ public void addUndirectedEdge(final GridNode first,final GridNode second){
+       Node v1 = current;
+       Node v2 = current;
+       
+       while(current != null){
+          if(first.equals(current.item)){
+             v1 = current;
+           }
+           if(second.equals(current.item)){
+             v2 = current;
+             }
+             current = current.next;
+             }
+
+   
+   }
+ public void removeUndirectedEdge(final GridNode first, final GridNode second){
+   return removeUndirectedEdge(new Node(first,last));
+   }
+
+  class Main{
+     
+    GridGraph createRandomGridGraph(int n)
+    
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+}
